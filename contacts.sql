2e619ff2d536b6cf7d8074368b55b024d95dda1d
Carlos Mendez <calicci.gardachi@gmail.com>
2017-07-16 22:48:48 -0400

    added new db schema to repo

--- contacts.sql
+++ contacts.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Jul 13, 2017 at 01:39 PM
+-- Generation Time: Jul 17, 2017 at 04:47 AM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.10
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `contacts`
 --
+CREATE DATABASE IF NOT EXISTS `contacts` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `contacts`;
 
 -- --------------------------------------------------------
 
@@ -27,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 DROP TABLE IF EXISTS `contacts`;
-CREATE TABLE `contacts` (
+CREATE TABLE IF NOT EXISTS `contacts` (
   `contId` int(11) unsigned NOT NULL,
   `contFName` varchar(50) DEFAULT NULL,
   `contLName` varchar(50) DEFAULT NULL,
@@ -35,27 +37,46 @@ CREATE TABLE `contacts` (
   `contEmail` varchar(50) DEFAULT NULL,
   `contTitle` varchar(50) DEFAULT NULL,
   `contCo` varchar(50) DEFAULT NULL,
-  `contDept` varchar(50) DEFAULT NULL,
-  `username` varchar(50) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+  `contDept` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `contacts`
 --
 
-INSERT INTO `contacts` (`contId`, `contFName`, `contLName`, `contPhone`, `contEmail`, `contTitle`, `contCo`, `contDept`, `username`, `password`) VALUES
-(1, 'Daffy', 'Duck', '555-1234', 'daffy@wb.com', '', NULL, NULL, 'daffy', '96b70bd61bba64d060891c9b71298aa2'),
-(2, 'Elmer', 'Fudd', '555-1235', 'elmer@wb.com', '', NULL, NULL, 'elmer', '96b70bd61bba64d060891c9b71298aa2'),
-(3, 'Porky', 'Pig', '555-1236', 'porky@wb.com', '', NULL, NULL, 'porky', '96b70bd61bba64d060891c9b71298aa2'),
-(4, 'Bugs', 'Bunny', '555-1237', 'bugs@wb.com', '', NULL, NULL, 'bugs', '96b70bd61bba64d060891c9b71298aa2'),
-(5, 'Tweety', 'Bird', '555-1238', 'tweety@wb.com', '', NULL, NULL, 'tweety', '96b70bd61bba64d060891c9b71298aa2'),
-(6, 'Pepe', 'LePew', '555-1239', 'pepe@wb.com', '', NULL, NULL, 'pepe', '96b70bd61bba64d060891c9b71298aa2'),
-(7, 'Yosemite', 'Sam', '555-1240', 'yosemite@wb.com', '', NULL, NULL, 'yosemite', '96b70bd61bba64d060891c9b71298aa2'),
-(8, 'Foghorn', 'Leghorn', '555-1241', 'foghorn@wb.com', '', NULL, NULL, 'foghorn', '96b70bd61bba64d060891c9b71298aa2'),
-(9, 'Marvin', 'Martian', '555-1242', 'marvin@wb.com', '', NULL, NULL, 'marvin', '96b70bd61bba64d060891c9b71298aa2'),
-(10, 'Road', 'Runner', '555-1243', 'beepbeep@wb.com', 'Chief Coyote Frustrater', 'Border Patrol', 'Pursuit Operations', 'beepbeep', '96b70bd61bba64d060891c9b71298aa2'),
-(11, 'Wiley', 'Coyote', '555-1244', 'wiley@wb.com', '', NULL, NULL, 'wiley', '96b70bd61bba64d060891c9b71298aa2');
+INSERT INTO `contacts` (`contId`, `contFName`, `contLName`, `contPhone`, `contEmail`, `contTitle`, `contCo`, `contDept`) VALUES
+(27, 'test', 'bob', '1234567980', 'me@test.com', '', NULL, NULL),
+(28, 'test', 'bob', '1234567980', 'me@test.com', '', NULL, NULL),
+(29, 'test', 'bob', '1234567980', 'me@test.com', '', NULL, NULL),
+(30, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(31, 'Test', 'Best', '12345647890', 'me@test.com', 'tester', NULL, NULL),
+(32, 'Test', 'Best', '12345647890', 'me@test.com', 'tester', 'tester', 'test'),
+(33, 'Test', 'Best', '1234567980', 'me@test.com', 'tester', 'tester', 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `userId` int(11) unsigned NOT NULL,
+  `userFName` varchar(50) DEFAULT NULL,
+  `userLName` varchar(50) DEFAULT NULL,
+  `userPhone` varchar(50) DEFAULT NULL,
+  `userEmail` varchar(50) DEFAULT NULL,
+  `userTitle` varchar(50) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userId`, `userFName`, `userLName`, `userPhone`, `userEmail`, `userTitle`, `username`, `password`) VALUES
+(3, 'tester', 'test', '1234567890', 'me@me.com', 'tester', 'root', '6378c16318e6edac49f80e776854e9ef');
 
 --
 -- Indexes for dumped tables
@@ -68,6 +89,12 @@ ALTER TABLE `contacts`
   ADD PRIMARY KEY (`contId`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userId`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -75,7 +102,12 @@ ALTER TABLE `contacts`
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `contId` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
+  MODIFY `contId` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=34;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userId` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
